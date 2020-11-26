a0997c0b98230e6f18bfcdbe067319a95e046cb8
gammelalf <31348226+gammelalf@users.noreply.github.com>
2020-11-26 07:53:03 +0100

    Added a db-based Dictionary and added it to ItemModel

--- component_organizer/backend/models.py
+++ component_organizer/backend/models.py
@@ -4,11 +4,60 @@ from django.db import models
 from django.core.validators import MinValueValidator
 
 
-class ItemModel(models.Model):
-    name = models.CharField(max_length=255, default="")
+class Dictionary(models.Model):
+
+    def __getitem__(self, key):
+        return self.keyvaluepair_set.get(key=key).value
+
+    def __setitem__(self, key, value):
+        try:
+            kvp = self.keyvaluepair_set.get(key=key)
+
+        except KeyValuePair.DoesNotExist:
+            KeyValuePair.objects.create(container=self, key=key, value=value)
+
+        else:
+            kvp.value = value
+            kvp.save()
+
+    def __delitem__(self, key):
+        try:
+            kvp = self.keyvaluepair_set.get(key=key)
+
+        except KeyValuePair.DoesNotExist:
+            raise KeyError
+
+        else:
+            kvp.delete()
+
+    def iterkeys(self):
+        return iter(kvp.key for kvp in self.keyvaluepair_set.all())
+
+    __iter__ = iterkeys
+
+    def iteritems(self):
+        return iter((kvp.key, kvp.value) for kvp in self.keyvaluepair_set.all())
+
+    def keys(self):
+        return [kvp.key for kvp in self.keyvaluepair_set.all()]
+
+    def items(self):
+        return [(kvp.key, kvp.value) for kvp in self.keyvaluepair_set.all()]
+
+    def clear(self):
+        self.keyvaluepair_set.all().delete()
 
     def __str__(self):
-        return self.name
+        return f"{dict(self.items())}"
+
+
+class KeyValuePair(models.Model):
+    container = models.ForeignKey(Dictionary, db_index=True, on_delete=models.CASCADE)
+    key = models.CharField(max_length=255, db_index=True)
+    value = models.CharField(max_length=255, db_index=True)
+
+    def __str__(self):
+        return f"{self.key}={self.value}"
 
 
 class ContainerModel(models.Model):
@@ -28,8 +77,26 @@ class ContainerModel(models.Model):
 
 class ItemLocationModel(models.Model):
     parent = models.ForeignKey("ContainerModel", on_delete=models.CASCADE)
-    item = models.ForeignKey(ItemModel, on_delete=models.CASCADE)
     number = models.PositiveIntegerField(validators=[MinValueValidator(1)], default=1)
 
+    @property
+    def path(self):
+        return self.parent.path
+
+
+def _create_default_dict() -> int:
+    dictionary = Dictionary()
+    dictionary.save()
+    return dictionary.id
+
+
+class ItemModel(models.Model):
+    """
+    Can be subclassed for templates
+    """
+    name = models.CharField(max_length=255, default="")
+    custom_values = models.ForeignKey(Dictionary, on_delete=models.CASCADE, default=_create_default_dict)
+    locations = models.ManyToManyField(ItemLocationModel)
+
     def __str__(self):
-        return f"Location of {self.number} x {self.item}"
+        return self.name
