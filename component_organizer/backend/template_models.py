8de756d4246cb9b047883a8bc153d65e89376da6
myOmikron <accounts@omikron.dev>
2020-11-26 23:41:41 +0100

    Splitted Resistor in BaseResistor and Resistor, added Potentiometer

--- component_organizer/backend/template_models.py
+++ component_organizer/backend/template_models.py
@@ -5,7 +5,6 @@ from backend.helper import UnicodeEscape
 
 
 class ElectronicTemplate(AbstractItemModel):
-
     class Meta:
         abstract = True
 
@@ -14,12 +13,28 @@ class ElectronicTemplate(AbstractItemModel):
     mounting = CharField(choices=[("THT", "THT"), ("SMD", "SMD"), ("Other", "Other")], default="THT", max_length=255)
 
 
-class Resistor(ElectronicTemplate):
-    resistance = FloatField(default=0)
+class BaseResistor(ElectronicTemplate):
+    class Meta:
+        abstract = True
+
     tolerance = FloatField(default=0, blank=True)
     max_power_dissipation = FloatField(default=0, blank=True)
 
+
+class Resistor(BaseResistor):
+    resistance = FloatField(default=0)
+
     def __str__(self):
         return "".join([f"Resistor {self.resistance} {UnicodeEscape.OHM}",
+                        f", {UnicodeEscape.PLUS_MINUS}{self.tolerance} %" if self.tolerance != 0 else "",
+                        f", max {self.max_power_dissipation} W" if self.max_power_dissipation != 0 else ""])
+
+
+class Potentiometer(BaseResistor):
+    min_resistance = FloatField(default=0)
+    max_resistance = FloatField(default=0)
+
+    def __str__(self):
+        return "".join([f"Potentiometer {self.min_resistance}-{self.max_resistance} {UnicodeEscape.OHM}",
                         f", {UnicodeEscape.PLUS_MINUS}{self.tolerance} %" if self.tolerance != 0 else "",
                         f", max {self.max_power_dissipation} W" if self.max_power_dissipation != 0 else ""])
