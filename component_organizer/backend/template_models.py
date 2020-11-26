9ace03bc740b833c48a239b14b234e6f58d64cb3
myOmikron <accounts@omikron.dev>
2020-11-26 22:47:27 +0100

    Added mounting option to EletronicTemplate, made a few fields blankable, added max_power_dissipation to resistor

--- component_organizer/backend/template_models.py
+++ component_organizer/backend/template_models.py
@@ -8,10 +8,17 @@ class ElectronicTemplate(AbstractItemModel):
     class Meta:
         abstract = True
 
-    schematic_symbol_path = CharField(default="", max_length=1024)
-    datasheet_path = CharField(default="", max_length=1024)
+    schematic_symbol_path = CharField(default="", max_length=1024, blank=True)
+    datasheet_path = CharField(default="", max_length=1024, blank=True)
+    mounting = CharField(choices=[("THT", "THT"), ("SMD", "SMD"), ("Other", "Other")], default="THT", max_length=255)
 
 
 class Resistor(ElectronicTemplate):
     resistance = FloatField(default=0)
-    tolerance = FloatField(default=0)
+    tolerance = FloatField(default=0, blank=True)
+    max_power_dissipation = FloatField(default=0, blank=True)
+
+    def __str__(self):
+        return ", ".join([f"Resistor {self.resistance} Ohm",
+                          f"{self.tolerance} %" if self.tolerance != 0 else None,
+                          f"max {self.max_power_dissipation} W" if self.max_power_dissipation != 0 else None])
