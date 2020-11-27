3993bad8242ceb8c227c7b231544b9eb9b10e53c
myOmikron <accounts@omikron.dev>
2020-11-27 02:10:18 +0100

    Added decoraters to ModelAdmins to register them

--- component_organizer/backend/admin.py
+++ component_organizer/backend/admin.py
@@ -4,61 +4,59 @@ from django.contrib.auth.models import Group
 from backend.models import *
 
 
+@admin.register(KeyValuePair)
 class KeyValuePairAdmin(admin.ModelAdmin):
     list_display = ("key", "value")
 
 
+@admin.register(ContainerModel)
 class ContainerModelAdmin(admin.ModelAdmin):
     list_display = ("parent", "name")
 
 
+@admin.register(ItemLocationModel)
 class ItemLocationModelAdmin(admin.ModelAdmin):
     list_display = ("parent", "amount", "path")
 
 
+@admin.register(Resistor)
 class ResistorAdmin(admin.ModelAdmin):
     list_display = ("__str__", "resistance", "tolerance", "max_power_dissipation", "mounting")
 
 
+@admin.register(Potentiometer)
 class PotentiometerAdmin(admin.ModelAdmin):
     list_display = ("__str__", "min_resistance", "max_resistance", "tolerance", "max_power_dissipation", "mounting")
 
 
+@admin.register(Thermistor)
 class ThermistorAdmin(admin.ModelAdmin):
     list_display = ("__str__", "thermistor_type", "tolerance", "max_power_dissipation", "mounting")
 
 
+@admin.register(Varistor)
 class VaristorAdmin(admin.ModelAdmin):
     list_display = ("__str__", "breakdown_voltage", "tolerance", "max_power_dissipation", "mounting")
 
 
+@admin.register(MagnetoResistor)
 class MagnetoResistorAdmin(admin.ModelAdmin):
     list_display = ("__str__", "tolerance", "max_power_dissipation", "mounting")
 
 
+@admin.register(PhotoResistor)
 class PhotoResistorAdmin(admin.ModelAdmin):
     list_display = ("__str__", "tolerance", "max_power_dissipation", "mounting")
 
 
+@admin.register(ShuntResistor)
 class ShuntResistorAdmin(admin.ModelAdmin):
     list_display = ("__str__", "max_current", "tolerance", "max_power_dissipation", "mounting")
 
 
+@admin.register(Capacitor)
 class CapacitorAdmin(admin.ModelAdmin):
     list_display = ("__str__", "capacitor_type", "capacitance", "max_voltage", "max_temperature", "mounting")
 
 
 admin.site.unregister(Group)
-
-admin.site.register(KeyValuePair, KeyValuePairAdmin)
-admin.site.register(ContainerModel, ContainerModelAdmin)
-admin.site.register(ItemLocationModel, ItemLocationModelAdmin)
-admin.site.register(ItemModel)
-admin.site.register(Resistor, ResistorAdmin)
-admin.site.register(Potentiometer, PotentiometerAdmin)
-admin.site.register(Thermistor, ThermistorAdmin)
-admin.site.register(Varistor, VaristorAdmin)
-admin.site.register(MagnetoResistor, MagnetoResistorAdmin)
-admin.site.register(PhotoResistor, PhotoResistorAdmin)
-admin.site.register(ShuntResistor, ShuntResistorAdmin)
-admin.site.register(Capacitor, CapacitorAdmin)
