2509bd855a94aa272336d7c90bee1bf85cd1dc48
gammelalf <31348226+gammelalf@users.noreply.github.com>
2020-11-30 22:54:56 +0100

    Drafted directory browsing

--- component_organizer/frontend/urls.py
+++ component_organizer/frontend/urls.py
@@ -1,2 +1,8 @@
+from django.urls import path
+
+from frontend.views import BrowserView
+
+
 urlpatterns = [
+    path("browse/<int:ct>", BrowserView.as_view())
 ]
--- component_organizer/frontend/views.py
+++ component_organizer/frontend/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpRequest, Http404
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView
 
-# Create your views here.
+from backend.models import Container, ItemLocation
+
+
+class BrowserView(TemplateView):
+    template_name = "frontend/browser.html"
+
+    def get(self, request: HttpRequest, ct: int = 1, *args, **kwargs):
+        print(ct)
+        container = get_object_or_404(Container, id=ct)
+
+        children = list(map(
+            lambda x: (x.id, x.name),
+            Container.objects.filter(parent=container).all()
+        ))
+
+        return render(
+            request,
+            self.template_name,
+            {
+                "path": container.path,
+                "containers": children
+            })
