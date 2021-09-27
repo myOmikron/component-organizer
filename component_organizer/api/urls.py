38499c59dc5b5eb91550f04cd9b6bccceaca5bcb
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-09-27 21:52:32 +0200

    Added item api endpoint

--- component_organizer/api/urls.py
+++ component_organizer/api/urls.py
@@ -6,4 +6,6 @@ from api.views import *
 urlpatterns = [
     path("template/<int:pk>", ItemTemplateView.as_view(http_method_names=["get", "put", "delete"])),
     path("template", ItemTemplateView.as_view(http_method_names=["get", "post"])),
+    path("item/<int:pk>", ItemView.as_view(http_method_names=["get", "put", "delete"])),
+    path("item", ItemView.as_view(http_method_names=["get", "post"])),
 ]
--- component_organizer/api/views.py
+++ component_organizer/api/views.py
@@ -6,11 +6,105 @@ from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.decorators.csrf import csrf_exempt
 
-from backend.models import StringValue, ItemTemplate
+from backend.models import StringValue, ItemTemplate, Item, Category
+
+
+def check_params(data: dict, parameters: list[tuple[str, type]]):
+    for name, dtype in parameters:
+        if name not in data:
+            return JsonResponse({"success": False, "error": f"Missing parameter: {name}"}, status=400)
+        if dtype and not isinstance(data[name], dtype):
+            return JsonResponse({"success": False, "error": "Parameter '{name}' must be of type '{dtype}'"}, status=400)
+    else:
+        return None
 
 
 class ApiAuth(LoginRequiredMixin, View):
     pass
+
+
+class ItemView(View):
+
+    @staticmethod
+    def item2dict(item: Item, expand_template=True):
+        return {
+            "id": item.id,
+            "category": item.category_id,
+            "template": {
+                "id": item.template.id,
+                "name": item.template.name_format,
+                "fields": item.template.get_fields()
+            } if expand_template else item.template_id,
+            "fields": dict(item.items())
+        }
+
+    def post(self, request, *args, **kwargs):
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return JsonResponse({"success": False, "error": "Couldn't parse json"}, status=405)
+
+        check_params(data, [("category", int), ("template", int), ("fields", dict)])
+
+        if not ItemTemplate.objects.filter(id=data["template"]).exists():
+            return JsonResponse({"success": False, "error": "Unknown template"}, status=404)
+        if not Category.objects.filter(id=data["category"]).exists():
+            return JsonResponse({"success": False, "error": "Unknown category"}, status=404)
+
+        item = Item.objects.create(category_id=data["category"], template_id=data["template"])
+        for key, value in data["fields"].items():
+            item[key] = value
+        item.save()
+
+        return self.item2dict(item)
+
+    def get(self, request, pk=None, *args, **kwargs):
+        if pk is None:
+            return JsonResponse(
+                [self.item2dict(item, False) for item in Item.objects.all()],
+                status=200, safe=False
+            )
+        else:
+            try:
+                return JsonResponse(self.item2dict(Item.objects.get(id=pk)))
+            except Item.DoesNotExist:
+                return JsonResponse({"success": False, "error": "Unknown item"}, status=404)
+
+    def put(self, request, *args, pk=None, **kwargs):
+        try:
+            item: Item = Item.objects.get(id=pk)
+        except Item.DoesNotExist:
+            return JsonResponse({"success": False, "error": "Unknown item"}, status=404)
+
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return JsonResponse({"success": False, "error": "Couldn't parse json"}, status=405)
+
+        if "template" in data and ItemTemplate.objects.filter(id=data["template"]).exists():
+            item.template_id = data["template"]
+        if "category" in data and Category.objects.filter(id=data["category"]).exists():
+            item.category_id = data["category"]
+        if "fields" in data and isinstance(data["fields"], dict):
+            for key, value in data["fields"].items():
+                item[key] = value
+            current_fields = list(data["fields"].keys()) + item.template.get_fields()
+            for key in item.keys():
+                if key not in current_fields:
+                    del item[key]
+        item.save()
+
+        return self.item2dict(item)
+
+    def delete(self, request, *args, pk=None, **kwargs):
+        try:
+            item: Item = Item.objects.get(id=pk)
+        except Item.DoesNotExist:
+            return JsonResponse({"success": False, "error": "Unknown item"}, status=404)
+
+        item.delete()
+
+        return JsonResponse({"success": True}, status=200)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -18,28 +112,21 @@ class ItemTemplateView(View):
 
     @staticmethod
     def template2dict(template: ItemTemplate):
-        return {"id": template.id, "name": template.name_format, "fields": [f.value for f in template.fields.all()]}
+        return {"id": template.id, "name": template.name_format, "fields": template.get_fields()}
 
     def post(self, request, *args, **kwargs):
-        # Parse parameters
         try:
             data = json.loads(request.body)
         except json.JSONDecodeError:
             return JsonResponse({"success": False, "error": "Couldn't parse json"}, status=405)
 
-        # Check parameters
-        if "name" not in data:
-            return JsonResponse({"success": False, "error": "Missing attribute: name"})
-        if "fields" not in data:
-            return JsonResponse({"success": False, "error": "Missing attribute: fields"})
+        check_params(data, [("name", str), ("fields", list)])
 
-        # Create new object
         template: ItemTemplate = ItemTemplate.objects.create(name_format=data["name"])
         for field in data["fields"]:
             if isinstance(field, str):
                 template.fields.add(StringValue.get(field))
 
-        # Return new object
         return JsonResponse(
             {"success": True, "result": self.template2dict(template)},
             status=200
