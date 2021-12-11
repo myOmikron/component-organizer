af53f59a3c31896a56245e91c9eacacbd3dc43bc
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-12-11 22:30:49 +0100

    Removed old obsolete code

--- component_organizer/frontend/views.py
+++ component_organizer/frontend/views.py
@@ -20,52 +20,8 @@ class ItemView(TemplateView):
     template_name = "frontend/items/edit.html"
 
     def get(self, request: HttpRequest, *args, item: int = None, **kwargs):
-        try:
-            item: Item = Item.objects.get(id=item)
-            item.populate()
-        except Item.DoesNotExist:
-            raise Http404
-
-        template_attrs = dict((attr, "") for attr in item.template.get_fields())
-        additional_attrs = dict()
-        for key, value in item.items():
-            if key in template_attrs:
-                template_attrs[key] = value
-            else:
-                additional_attrs[key] = value
-
-        return render(request=request, template_name=self.template_name, context={
-            "item": item,
-            "template_attrs": template_attrs.items(),
-            "additional_attrs": additional_attrs.items(),
-        })
-
-    def post(self, request: HttpRequest, *args, item: int = None, **kwargs):
-        try:
-            item: Item = Item.objects.get(id=item)
-        except Item.DoesNotExist:
-            raise Http404
-
-        for key, value in request.POST.items():
-            if key.startswith("set_"):
-                key = key[len("set_"):]
-
-                try:
-                    value = float(value)
-                except ValueError:
-                    pass
-
-                if key in item and item[key] == value:
-                    continue
-                else:
-                    item[key] = value
-
-            elif key.startswith("delete_"):
-                key = key[len("delete_"):]
-
-                del item[key]
-
-        return HttpResponseRedirect(request.path)
+        get_object_or_404(Item, id=item)
+        return render(request=request, template_name=self.template_name)
 
 
 class ItemListView(TemplateView):
@@ -160,4 +116,4 @@ class CreateContainerView(CreateView):
 
 
 def redirect_to_root(*args, **kwargs):
-    return HttpResponseRedirect(Container.objects.get(id=0).url)
+    return HttpResponseRedirect("/items")
